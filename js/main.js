4089694c499f96e181c36cdc2de7facd97d452c6
Mariah Alderson <mariah.alderson@gmail.com>
2018-10-03 17:11:34 -0400

    tablet media queries

--- js/main.js
+++ js/main.js
@@ -3,11 +3,7 @@ var hamburger = document.querySelector("#hamburger"),
     mainnav = document.querySelector("#navigation");
 
 //about us vars
-var //sm-cait = document.querySelector("#cait img"),
-    //sm-mar = document.querySelector("#mariah img"),
-    mdCait = "images/caitlin-med.png",
-    mdMar = "images/mariah-med.png",
-    links = Array.from(document.querySelectorAll(".aboutNames")),
+var links = Array.from(document.querySelectorAll(".aboutNames")),
     aboutBlurbs = Array.from(document.querySelectorAll(".aboutCopy")),
     tabletAbout = document.querySelector("#aboutText"),
     tabletimg = document.querySelector("#tabletImg");
@@ -21,23 +17,49 @@ function openmenu(){
 }
 
 
-//change about photo and text
-function aboutimg(e){
-
-    //link info
-    var clicked = links.indexOf(e.target);
-    var text = aboutBlurbs[clicked].innerHTML;
-
-    //change img
-    tabletImg.src = "images/" + e.target.dataset.name + "-med.png";
- 
-    //change blurb
-    tabletAbout.innerHTML = text;
-    
+function changeimg(e){
+//change img
+    setTimeout(function(){
+        tabletImg.src = "images/" + e.target.dataset.name + "-med.png";
+        tabletImg.classList.remove("fade");
+        tabletAbout.classList.remove("fade");
+    }, 200);
 }
+
+ 
+function changeblurb(index){
+//change blurb
+var text = aboutBlurbs[index].innerHTML;
+    tabletAbout.innerHTML = text;
+    tabletAbout.style.transform = "translateX(0px)";
+}
+
+
+//edit about me area
+function aboutchange(e){
+    //set up about area for change
+    links.forEach(link=>link.style.color = "#555");
+    e.target.style.color = "#999";
+    tabletImg.classList.add("fade");
+    tabletAbout.classList.add("fade");
+
+    //link info (index, get info)
+    var clicked = links.indexOf(e.target);
+    
+
+    //move links
+    var namelink = document.querySelector("#names");
+    namelink.classList.add("opentext");
+
+    changeimg(e);
+    changeblurb(clicked);
+}
+
+    
+
 
 hamburger.addEventListener('click', openmenu);
 links.forEach(link=>link.addEventListener('click', function(e){
     e.preventDefault();
-     aboutimg(e)
+     aboutchange(e);
 }));
